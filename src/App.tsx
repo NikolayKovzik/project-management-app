26a72db1fb8b11487ca283b3d8ae94e34ee2dec3
Nikolay <nikolay.kovzik@gmail.com>
2022-11-09 15:58:58 +0300

    feat: update eslint & typescript configs

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,26 @@ import './normalize.css';
 import './App.css';
 
 function App() {
+  const x = {
+    name: 'dfsdf',
+    surname: 32,
+  };
+
+  function bruh(y: { name: string; surname: number }): boolean {
+    let varr = false;
+    const mur = {
+      z: y,
+      rand: Math.random(),
+    };
+
+    if (mur.rand < 32) {
+      varr = true;
+      return varr;
+    }
+
+    return true;
+  }
+
   return <div className="App" />;
 }
 
