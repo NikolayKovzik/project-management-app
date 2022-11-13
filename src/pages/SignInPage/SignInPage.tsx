334a971eaaea61e2204e9b7f66abb1692241ecbc
Kirill Kovalev <kirill.kovalev1502@mail.ru>
2022-11-13 22:11:20 +0300

    feat: add styles to sign-in page

--- src/pages/SignInPage/SignInPage.tsx
+++ src/pages/SignInPage/SignInPage.tsx
@@ -1,6 +1,10 @@
 import React, { ReactElement } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 
+import signImage from '../../assets/images/authorization.png';
+
+import styles from './SignInPage.module.scss';
+
 function LoginPage(): ReactElement {
   const { state } = useLocation();
   const navigate = useNavigate();
@@ -13,12 +17,35 @@ function LoginPage(): ReactElement {
     } else navigate('/');
   }
   return (
-    <div>
-      <h1>Login Page</h1>
-      <input type="text" placeholder="login" />
-      <input type="text" placeholder="password" />
-      <Link to="/signup">I haven`t account</Link>
-    </div>
+    <section className={styles.signin}>
+      <div className="container">
+        <div className={styles.container}>
+          <div className={styles.mainContainer}>
+            <div className={styles.inputData}>
+              <p className={styles.title}>Log in your account</p>
+              <input type="text" placeholder="login" />
+              <input type="text" placeholder="password" />
+              <div className={styles.btnsContainer}>
+                <button type="button" className={styles.signUpBtn}>
+                  Log in
+                </button>
+                <Link className={styles.helpBtn} to="/signup">
+                  I haven`t account
+                </Link>
+              </div>
+            </div>
+            <div className={styles.imageBackground}>
+              <img src={signImage} alt="sign-up" />
+              <div className={styles.imageTitleContainer}>
+                <p className={styles.imageTitle}>Hello,</p>
+                <p className={styles.imageTitle}>Welcome back!</p>
+              </div>
+              <p className={styles.imageSubTitle}>Log in your account on our platform</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
