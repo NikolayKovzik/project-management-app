efa8f704cdc60ca7bd9835fab72222be76a8ad1c
Kirill Kovalev <kirill.kovalev1502@mail.ru>
2022-11-13 11:36:49 +0300

    init: start 8-notfound-page-styling

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,17 +1,30 @@
 import React, { ReactElement } from 'react';
 import { Link } from 'react-router-dom';
 
-import './NotFoundPage.scss';
+import notfoundImage from '../../assets/images/notfound.png';
+
+import styles from './NotFoundPage.module.scss';
 
 function NotfoundPage(): ReactElement {
   return (
-    <section className="notfound-page">
-      <p className="notfound-page__warning">
-        404 Not found.{' '}
-        <Link to="/" className="notfound-page__home-link">
-          Back to home
-        </Link>
-      </p>
+    <section className={styles.notfound}>
+      <div className="container">
+        <div className={styles.container}>
+          <div className={styles.mainContainer}>
+            <img src={notfoundImage} alt="notfound" />
+            <div className={styles.info}>
+              <p className={styles.title}>Oops! Page not found.</p>
+              <p className={styles.subtitle}>The page you were looking for doesn&#39;t exist</p>
+              <p className={styles.advice}>
+                You may have mistyped the address or the page may have moved
+              </p>
+              <Link to="/" className={styles.btnHome}>
+                Back to home
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
