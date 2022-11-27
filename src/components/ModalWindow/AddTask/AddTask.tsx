1bc00b2c803157a139309ff2d585dcf1719bfdeb
Kirill Kovalev <kirill.kovalev1502@mail.ru>
2022-11-27 13:33:33 +0300

    feat: add create task form

---
+++ src/components/ModalWindow/AddTask/AddTask.tsx
@@ -0,0 +1,49 @@
+import React, { ReactElement, useState } from 'react';
+import { TaskCreateBody } from 'core/api/models';
+
+import { Props } from '../types/AddTask.types';
+
+import styles from './AddTask.module.scss';
+
+const AddTask = ({ toggleModalWindow, createTask }: Props): ReactElement => {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+
+  const changeInputTitle = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setTitle(e.target.value);
+  };
+  const changeInputDescription = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setDescription(e.target.value);
+  };
+
+  const addTask = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>): void => {
+    const task: TaskCreateBody = {
+      title,
+      order: 0,
+      description,
+      userId: '',
+      users: [],
+    };
+    createTask?.(task);
+    toggleModalWindow?.(e);
+  };
+
+  return (
+    <>
+      <p className={styles.title}>Create new task</p>
+      <div className={styles.inputContainer}>
+        <p>Task title</p>
+        <input value={title} onChange={changeInputTitle} />
+      </div>
+      <div className={styles.inputContainer}>
+        <p>Task description</p>
+        <input value={description} onChange={changeInputDescription} />
+      </div>
+      <button className={styles.createButton} type="button" onClick={addTask}>
+        Add
+      </button>
+    </>
+  );
+};
+
+export default AddTask;
