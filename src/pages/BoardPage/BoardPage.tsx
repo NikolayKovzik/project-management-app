b928913755f4745aae148af6b0e7e9083d3543f9
DarkCrew <67704676+DarkCrew@users.noreply.github.com>
2022-11-27 22:04:19 +0300

    feat: add custom loader (#75)

--- src/pages/BoardPage/BoardPage.tsx
+++ src/pages/BoardPage/BoardPage.tsx
@@ -6,14 +6,16 @@ import ColumnsApi from 'core/api/ColumnsApi';
 import { Column, ColumnBody, ColumnPostBody } from 'core/api/models';
 
 import ColumnItem from 'components/ColumnItem/ColumnItem';
+import Loader from 'components/Loader/Loader';
 import ModalWindow from 'components/ModalWindow/ModalWindow';
 
 import styles from './BoardPage.module.scss';
 
 const BoardPage = (): ReactElement => {
-  const [modalWindow, setModalWindow] = useState(false);
   const params = useParams();
+  const [modalWindow, setModalWindow] = useState(false);
   const [columns, setColumns] = useState<Column[]>([]);
+  const [loading, setLoading] = useState(false);
 
   const toggleModalWindow = (event: React.MouseEvent<HTMLElement, MouseEvent>): void => {
     event.preventDefault();
@@ -21,9 +23,11 @@ const BoardPage = (): ReactElement => {
   };
 
   useEffect(() => {
+    setLoading(true);
     const getAllColumns = async (): Promise<void> => {
       const getColumns = await ColumnsApi.getAllColumnsByBoardId(String(params.id));
       setColumns(getColumns.data);
+      setLoading(false);
     };
     getAllColumns();
   }, []);
@@ -43,38 +47,45 @@ const BoardPage = (): ReactElement => {
   };
 
   return (
-    <section className={styles.boards}>
-      <div className="container">
-        <div className={styles.container}>
-          <NavLink className={styles.backButton} to="/main">
-            <button type="button">&#5130;</button>
-          </NavLink>
-          <div className={styles.mainContainer}>
-            {columns.map((column: ColumnPostBody) => (
-              <ColumnItem boardId={String(params.id)} column={column} deleteColumn={deleteColumn} />
-            ))}
-            <div className={styles.addButton}>
-              <div className={styles.buttonAddContainer}>
-                <button
-                  className={styles.addColumnButton}
-                  type="button"
-                  onClick={toggleModalWindow}
-                >
-                  {modalWindow && (
-                    <ModalWindow
-                      type="createcolumn"
-                      toggleModalWindow={toggleModalWindow}
-                      createColumn={createColumn}
-                    />
-                  )}
-                  + Add column
-                </button>
+    <>
+      {loading && <Loader />}
+      <section className={styles.boards}>
+        <div className="container">
+          <div className={styles.container}>
+            <NavLink className={styles.backButton} to="/main">
+              <button type="button">&#5130;</button>
+            </NavLink>
+            <div className={styles.mainContainer}>
+              {columns.map((column: ColumnPostBody) => (
+                <ColumnItem
+                  boardId={String(params.id)}
+                  column={column}
+                  deleteColumn={deleteColumn}
+                />
+              ))}
+              <div className={styles.addButton}>
+                <div className={styles.buttonAddContainer}>
+                  <button
+                    className={styles.addColumnButton}
+                    type="button"
+                    onClick={toggleModalWindow}
+                  >
+                    {modalWindow && (
+                      <ModalWindow
+                        type="createcolumn"
+                        toggleModalWindow={toggleModalWindow}
+                        createColumn={createColumn}
+                      />
+                    )}
+                    + Add column
+                  </button>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
