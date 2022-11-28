1ee691d49c2e33046b12ab7f71b0d20372b1228c
Kirill Kovalev <kirill.kovalev1502@mail.ru>
2022-11-28 12:40:14 +0300

    feat: add ability to create column with api

--- src/components/ModalWindow/types/AddColumn.types.ts
+++ src/components/ModalWindow/types/AddColumn.types.ts
@@ -1,6 +1,6 @@
-import { Column } from 'core/api/models';
+import { ColumnBody } from 'core/api/models';
 
 export type Props = {
   toggleModalWindow?: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
-  createColumn?: (column: Column) => void;
+  createColumn?: (column: ColumnBody) => void;
 };
