486bc4703d05d2b4da1cef03bc1bd2427219e7bd
Kirill Kovalev <kirill.kovalev1502@mail.ru>
2022-12-01 20:36:21 +0300

    feat: add validation to sign-in form

--- src/pages/SignInPage/SignInPage.tsx
+++ src/pages/SignInPage/SignInPage.tsx
@@ -1,4 +1,6 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React, { ReactElement } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from 'store';
 import { sendLoginRequest } from 'store/authSlice';
@@ -19,18 +21,30 @@ const LoginPage = (): ReactElement => {
   // ***
   const prevLocation = navState?.from || '';
 
-  const submit = (): void => {
-    //*  REDUX THUNK using
-    dispatch(sendLoginRequest({ login: '123123', password: '123123' }));
-    // dispatch(sendLoginRequest({ login: 'Ilo7776', password: 'qwerty123' }));
-    // ***
-  };
-
   const handleSuccessSubmit = (): void => {
     // TODO redux global state isAuth = thue
     if (navState.from) {
       navigate(navState.from);
     } else navigate('/');
+  };
+
+  interface IFormInput {
+    login: string;
+    password: string;
+  }
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormInput>();
+
+  const onSubmit: SubmitHandler<IFormInput> = (data: IFormInput): void => {
+    dispatch(sendLoginRequest({ login: data.login, password: data.password }));
+    if (loginStatus === 'succeeded') {
+      navigate('/');
+    }
+    // dispatch(sendLoginRequest({ login: 'Ilo7776', password: 'qwerty123' }));
   };
 
   return (
@@ -40,25 +54,47 @@ const LoginPage = (): ReactElement => {
         <div className="container">
           <div className={styles.container}>
             <div className={styles.mainContainer}>
-              <div className={styles.inputData}>
+              <form className={styles.inputData} onSubmit={handleSubmit(onSubmit)}>
                 <p className={styles.title}>Log in your account</p>
                 <div className={styles.inputContainer}>
-                  <input type="text" placeholder="login" />
+                  <input
+                    {...register('login', {
+                      required: 'Required field',
+                      minLength: {
+                        value: 4,
+                        message: 'Login should contain minimum 3 symbols',
+                      },
+                    })}
+                    type="text"
+                    placeholder="login"
+                  />
+                  {errors.login && <p>{errors.login.message}</p>}
                   {loginStatus === 'failed' && <p>Wrong login or password</p>}
                 </div>
                 <div className={styles.inputContainer}>
-                  <input type="text" placeholder="password" />
+                  <input
+                    {...register('password', {
+                      required: 'Required field',
+                      minLength: {
+                        value: 6,
+                        message: 'Password should contain minimum 3 symbols',
+                      },
+                    })}
+                    type="text"
+                    placeholder="password"
+                  />
+                  {errors.password && <p>{errors.password.message}</p>}
                   {loginStatus === 'failed' && <p>Wrong login or password</p>}
                 </div>
                 <div className={styles.btnsContainer}>
-                  <button type="button" className={styles.signUpBtn} onClick={submit}>
+                  <button type="submit" className={styles.signUpBtn}>
                     Log in
                   </button>
                   <Link className={styles.helpBtn} to="/signup">
                     I haven`t account
                   </Link>
                 </div>
-              </div>
+              </form>
               <div className={styles.imageBackground}>
                 <img src={signImage} alt="sign-up" />
                 <div className={styles.imageTitleContainer}>
